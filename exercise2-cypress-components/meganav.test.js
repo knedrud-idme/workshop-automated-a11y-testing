fe5fb387a930a3a5aa44334de923b3b5432d4171
Kirsten Nedrud <kirsten.nedrud@id.me>
2023-10-27 11:53:41 -0400

    Add test for open submenu links

--- exercise2-cypress-components/meganav.test.js
+++ exercise2-cypress-components/meganav.test.js
@@ -13,4 +13,24 @@ describe('MegaNav', () => {
 
         cy.focused().should('have.attr', 'aria-expanded', 'true')
     })
+
+    it('submenus should not be reachable when closed', () => {
+        cy.get('[data-testid="megamenu-section1"]').focus().realPress('Tab')
+
+        cy.focused().should('not.have.attr', '[data-testid="link-0"]')
+
+        cy.focused().then(($el) => {
+            expect($el).to.have.attr('data-testid', 'megamenu-section2')
+        })
+    })
+
+    it('submenu links should be reachable via keyboard when open', () => {
+        cy.get('[data-testid="megamenu-section1"]').focus().click()
+
+        cy.focused().realPress('Tab')
+
+        cy.focused().then(($el) => {
+            expect($el).to.have.attr('data-testid', 'link-0')
+        })
+    })
 })
