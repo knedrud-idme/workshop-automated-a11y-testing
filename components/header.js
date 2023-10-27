a303830c94525fead8c543eed80aa4353eb284e8
Kirsten Nedrud <kirsten.nedrud@id.me>
2023-10-27 14:51:09 -0400

    Add aria-current

--- components/header.js
+++ components/header.js
@@ -1,15 +1,22 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+
 import "./styles/header.scss"
 
 import CampSpotsLogo from "images/icons/camp-spots-logo.svg"
 import MegaNav from "./meganav/"
 
 const Header = ({inert}) => {
+    const [currentPathname, setCurrentPathname] = useState(null)
+
+    useEffect(() => {
+        setCurrentPathname(document.location.pathname);
+    },[])
+
     return (
         <header id="header" inert={inert}>
             <div id="header-nav">
                 <div id="header-logo">
-                    <a href="/" className="header-main-item">
+                    <a href="/" className="header-main-item" aria-current={currentPathname === '/' ? 'page' : null}>
                         <span className="logo-img">
                             <img src={CampSpotsLogo} alt=""/>
                         </span>
--- exercise3-cypress-integration/HomePage.test.js
+++ exercise3-cypress-integration/HomePage.test.js
@@ -8,6 +8,10 @@ describe('HomePage', () => {
         cy.get('#header').contains('CampSpots')
     })
 
+    it('should have aria-current on the logo link', () => {
+        cy.get('#header-logo a').should('have.attr', 'aria-current', 'page')
+    })
+
     it('should have no accessibility violations', () => {
         cy.checkA11y({
             exclude: ['div#portal-root']
